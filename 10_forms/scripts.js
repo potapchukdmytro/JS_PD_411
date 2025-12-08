fb3227f1b489311af2730cf32846611595609c73
Dmytro Potapchuk <40177161+potapchukdmytro@users.noreply.github.com>
2025-12-08 17:32:23 +0200

    validation

--- 10_forms/scripts.js
+++ 10_forms/scripts.js
@@ -2,6 +2,50 @@ function loginFormHandle(event) {
     event.preventDefault(); // зупиняє виконання форми
 
     const form = event.target;
+    const emailError = document.getElementById("emailError");
+    const passwordError = document.getElementById("passwordError");
+
+    // const email = form["email"].value;
+    // const password = form["password"].value;
+
+    // const loginData = {
+    //     email: email,
+    //     password: password
+    // };
+
+    // console.log(loginData);
+
+    const formData = new FormData(form);
+    const values = Object.fromEntries(formData.entries());
+    console.log(values);   
+
+    if(!validateEmail(values.email)) {
+        emailError.innerText = "Невірний формат пошти"
+        emailError.hidden = false;
+    } else {
+        emailError.hidden = true;
+    }
+
+    if(values.password.length < 6) {
+        passwordError.innerText = "Мінімальна довжина паролю 6 символів"
+        passwordError.hidden = false;
+    } else {
+        passwordError.hidden = true;
+    }
+}
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+function formSubmit(event) {
+    event.preventDefault(); // зупиняє виконання форми
+
+    const form = event.target;
 
     // const email = form["email"].value;
     // const password = form["password"].value;
@@ -18,8 +62,6 @@ function loginFormHandle(event) {
     const checkbox = form["rules"];
     console.log(checkbox.checked);
     console.log(values);   
-    
-    form.reset();
 }
 
 function selectPicture(event) {
