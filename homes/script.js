c60d928d68fbb45d083c031fa85d564a32e5064e
Dmytro Potapchuk <40177161+potapchukdmytro@users.noreply.github.com>
2025-12-08 15:22:23 +0200

    home colored text

--- homes/script.js
+++ homes/script.js
@@ -1,17 +1,24 @@
-const url = "https://dog.ceo/api/breeds/list/all";
-fetchDogBreeds();
+// const url = "https://dog.ceo/api/breeds/list/all";
+// fetchDogBreeds();
 
-async function fetchDogBreeds() {
-    try {
-        const response = await fetch(url);   
-        const data = await response.json();
-        const breeds = data.message;
-        const list = document.getElementById("list");
+// async function fetchDogBreeds() {
+//     try {
+//         const response = await fetch(url);   
+//         const data = await response.json();
+//         const breeds = data.message;
+//         const list = document.getElementById("list");
 
-        for (const item in breeds) {
-            list.innerHTML += `<li>${item}</li>`;
-        }
-    } catch (error) {
-        console.error(error);
-    }
+//         for (const item in breeds) {
+//             list.innerHTML += `<li>${item}</li>`;
+//         }
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+function changeColorHandle(event) {
+    const button = event.target;
+    const text = document.getElementById("text");
+    // text.style.color = button.style.backgroundColor;
+    text.style.color = button.dataset.color;
 }
