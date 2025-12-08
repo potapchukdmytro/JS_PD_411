70032cab52076618f29ea66366f70fcb0c6fa356
Dmytro Potapchuk <40177161+potapchukdmytro@users.noreply.github.com>
2025-12-08 17:21:12 +0200

    forms

--- 10_forms/scripts.js
+++ 10_forms/scripts.js
@@ -17,5 +17,37 @@ function loginFormHandle(event) {
     const values = Object.fromEntries(formData.entries());
     const checkbox = form["rules"];
     console.log(checkbox.checked);
-    console.log(values);
+    console.log(values);   
+    
+    form.reset();
+}
+
+function selectPicture(event) {
+    const input = event.target;
+    const images = input.files;
+    const container = document.getElementById("preview");
+    container.innerHTML = "";
+
+    for (const image of images) {
+        // <img width="100" src="" alt="picture" hidden id="picture">
+        const url = URL.createObjectURL(image);
+        container.innerHTML += `<img width="100" src="${url}" alt="${image.name}">`;
+    }
+
+    // if(picture) {
+    //     const url = URL.createObjectURL(picture);
+    //     const img = document.getElementById("picture");
+    //     img.src = url;
+    //     img.hidden = false;
+    // }
+}
+
+function selectAudio(event) {
+    const audio = event.target.files[0];
+
+    if(audio) {
+        const url = URL.createObjectURL(audio);
+        const player = document.getElementById("audioPlayer");
+        player.src = url;
+    }
 }
