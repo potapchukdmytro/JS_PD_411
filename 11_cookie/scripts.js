b4ee75128631e678de7454a39a93a51790e8ccb5
Dmytro Potapchuk <40177161+potapchukdmytro@users.noreply.github.com>
2025-12-10 16:21:13 +0200

    Set cookie

---
+++ 11_cookie/scripts.js
@@ -0,0 +1,13 @@
+// cookie
+
+function setCookie() {
+    document.cookie = "username=dima;path=/;expires=Thu, 11 Dec 2025 16:56:32 GMT"
+
+    let date = new Date();
+    date.setDate(date.getDate() + 1)
+    document.cookie = "login=admin;path=/;expires=" + date.toUTCString()
+
+    date = new Date();
+    date.setMinutes(date.getMinutes() + 1)
+    document.cookie = "tmp=value;path=/;expires=" + date.toUTCString()
+}
